0458b7cf5c9a9c94cb49a8589b338d32f06a2645
Angleby <angleby@gmail.com>
2019-03-18 16:06:09 -0300

    Ajustando para consumo no frontend

--- projeto-backend/src/main/java/com/stefanini/projeto/repository/CarteiraRepository.java
+++ projeto-backend/src/main/java/com/stefanini/projeto/repository/CarteiraRepository.java
@@ -1,9 +1,13 @@
 package com.stefanini.projeto.repository;
 
+
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.data.repository.CrudRepository;
 
 import com.stefanini.projeto.model.Carteira;
 
 public interface CarteiraRepository extends CrudRepository<Carteira, Long>{
-
+	List<Carteira> findAllByNomeContaining(String nome);
 }
